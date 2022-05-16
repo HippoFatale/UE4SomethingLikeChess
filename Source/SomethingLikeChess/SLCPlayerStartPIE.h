4f49bf99cd256ea165bd8068cb57588878cba5e3
hippo7665 <hippo7665@gmail.com>
2022-05-16 23:05:00 +0900

    destroy overlapping spawned pieces(not working)

---
+++ Source/SomethingLikeChess/SLCPlayerStartPIE.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/PlayerStartPIE.h"
+#include "SLCPlayerStartPIE.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOMETHINGLIKECHESS_API ASLCPlayerStartPIE : public APlayerStartPIE
+{
+	GENERATED_BODY()
+	
+};
